1290f11d9e7b90cfb639c34f18d406231a1ba0e0
Fannar Magnússon <fannar@kth.se>
2016-11-10 18:27:54 +0100

    tour guide agent virtual tour service added

--- HW1-SmartMuseum/src/main/java/com/fmttsi/smartmuseum/TourGuideAgent.java
+++ HW1-SmartMuseum/src/main/java/com/fmttsi/smartmuseum/TourGuideAgent.java
@@ -1,10 +1,23 @@
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import java.util.List;
 
 public class TourGuideAgent extends Agent
 {
+    // TODO: Find all Curators from DF
+    private AID curatorAgent = new AID("curatorAgent", AID.ISLOCALNAME);
+
     protected void setup()
     {
         System.out.println("TourGuideAgent " + getAID().getName() + " is ready.");
+
+        //add behavior to listen to virtual-tour requests from profiler agent
+        addBehaviour(new VirtualTourServer());
     }
 
     protected void takeDown()
@@ -12,4 +25,183 @@ public class TourGuideAgent extends Agent
         //Do necessary clean up here
         System.out.println("TourGuideAgent " + getAID().getName() + " terminating.");
     }
+
+    private class VirtualTourServer extends CyclicBehaviour
+    {
+        public void action()
+        {
+            MessageTemplate mt = MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.CFP),
+                    MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL));
+            ACLMessage msg = myAgent.receive(mt);
+
+            if(msg != null)
+            {
+                String interests = msg.getContent();
+                ACLMessage reply = msg.createReply();
+
+                //send content number of artifacts
+                addBehaviour(
+                        new GetArtifacts(
+                                interests,
+                                new ArtifactCallback()
+                                {
+                                    @Override
+                                    public void onGetArtifacts(List<Artifact> artifacts)
+                                    {
+                                        if(artifacts != null)
+                                        {
+                                            if(msg.getPerformative() == ACLMessage.CFP)
+                                            {
+                                                int artifactsCount = artifacts.size();
+
+                                                addBehaviour(new SendArtifactsCount(artifactsCount, reply));
+                                            }
+                                            else if(msg.getPerformative() == ACLMessage.ACCEPT_PROPOSAL)
+                                            {
+                                                addBehaviour(new SendVirtualTour(artifacts, reply));
+                                            }
+                                        }
+                                        else
+                                        {
+                                            reply.setPerformative(ACLMessage.REFUSE);
+                                            myAgent.send(reply);
+                                        }
+
+
+                                    }
+                                }
+                        )
+                );
+
+            }
+            else
+            {
+                block();
+            }
+        }
+    }
+
+    private class SendArtifactsCount extends OneShotBehaviour
+    {
+        private int artifactsCount;
+        private ACLMessage reply;
+
+        public SendArtifactsCount(int artifactsCount, ACLMessage reply)
+        {
+            this.artifactsCount = artifactsCount;
+            this.reply = reply;
+        }
+
+        public void action()
+        {
+            // Send the number of artifacts to the profiler
+            this.reply.setPerformative(ACLMessage.PROPOSE);
+            this.reply.setContent(String.valueOf(artifactsCount));
+            this.myAgent.send(reply);
+        }
+    }
+
+    private class SendVirtualTour extends OneShotBehaviour
+    {
+        private List<Artifact> artifacts;
+        private ACLMessage reply;
+
+        public SendVirtualTour(List<Artifact> artifacts, ACLMessage reply)
+        {
+            this.artifacts = artifacts;
+            this.reply = reply;
+        }
+
+        public void action()
+        {
+            this.reply.setPerformative(ACLMessage.INFORM);
+            //this.reply.setContentObject(null); //TODO : Format and serialize artifacts
+            this.myAgent.send(reply);
+        }
+    }
+
+    // TODO : For now we assume that the tour guide agent only knows of one curator agent
+    private class GetArtifacts extends Behaviour
+    {
+        private String interests;
+        private ArtifactCallback delegate;
+        private MessageTemplate mt;
+        private int step;
+
+        public GetArtifacts(String interests, ArtifactCallback delegate)
+        {
+            this.interests = interests;
+            this.delegate = delegate;
+            this.step = 0;
+        }
+
+        public void action()
+        {
+            switch (this.step)
+            {
+                case 0:
+                    // Send request to curator agent to get list of artifacts for given interests
+                    ACLMessage getListOfArtifactsMsg = new ACLMessage(ACLMessage.REQUEST);
+                    getListOfArtifactsMsg.addReceiver(curatorAgent);
+                    getListOfArtifactsMsg.setLanguage("English");
+                    getListOfArtifactsMsg.setContent(interests);
+                    getListOfArtifactsMsg.setConversationId("get-artifacts-for-interest");
+                    getListOfArtifactsMsg.setReplyWith("get-artifacts" + System.currentTimeMillis());
+                    myAgent.send(getListOfArtifactsMsg);
+
+                    this.mt = MessageTemplate.and(MessageTemplate.MatchConversationId("get-artifacts-for-interest"),
+                            MessageTemplate.MatchInReplyTo(getListOfArtifactsMsg.getReplyWith()));
+
+                    step = 1;
+                    break;
+                case 1:
+                    // Receive list of artifacts from curator agent
+                    ACLMessage reply = myAgent.receive(this.mt);
+
+                    if(reply != null)
+                    {
+                        if(reply.getPerformative() == ACLMessage.REFUSE)
+                        {
+                            //No artifacts available
+                            if(this.delegate != null)
+                                this.delegate.onGetArtifacts(null);
+                        }
+                        else
+                        {
+                            try
+                            {
+                                if(this.delegate != null)
+                                {
+                                    List<Artifact> artifacts = (List<Artifact>) reply.getContentObject();
+                                    this.delegate.onGetArtifacts(artifacts);
+                                }
+                            }
+                            catch(Exception ex)
+                            {
+                                ex.printStackTrace();
+                            }
+                        }
+
+                        step = 2;
+                    }
+                    else
+                    {
+                        block();
+                    }
+                    break;
+            }
+        }
+
+        public boolean done()
+        {
+            return step == 2;
+        }
+    }
+
+
+    private interface ArtifactCallback
+    {
+        void onGetArtifacts(List<Artifact> artifacts);
+    }
+
 }
