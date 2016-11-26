3acf128167bd8047f529000c569038c705fd662c
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-11-26 15:50:20 +0100

    trying to find predecessor and successor queens

--- hw3-nqueens/src/main/java/agents/QueenAgent.java
+++ hw3-nqueens/src/main/java/agents/QueenAgent.java
@@ -2,8 +2,10 @@ package agents;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.WakerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.Property;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 
@@ -11,16 +13,46 @@ import java.util.ArrayList;
 
 public class QueenAgent extends Agent
 {
-    private ArrayList<AID> queens;
+    private AID predecessor;
+    private AID successor;
+    /**
+     * My ID.
+     * */
+    private int id;
+    /**
+     * Total number of queens. Will be arranged on an nxn matrix.
+     * */
+    private int n;
 
     protected void setup()
     {
-        this.queens = new ArrayList<>();
+        // Get command line arguments
+        Object[] args = getArguments();
+        if (args != null && args.length == 2)
+        {
+            try
+            {
+                this.id = Integer.parseInt((String)args[0]);
+                this.n = Integer.parseInt((String)args[1]);
+            }
+            catch (Exception ex)
+            {
+                System.err.println(ex);
+                System.out.println(getName() + " - Invalid command line arguments. Should be: [ID] [N]");
+                return;
+            }
+        }
+        else
+        {
+            System.out.println(getName() + " - Invalid command line arguments. Should be: [ID] [N]");
+            return;
+        }
 
         registerQueenServices();
-        getQueens();
 
-        System.out.println("QueenAgent " + getAID().getName() + " is ready.");
+        this.addBehaviour(new InitWakerBehaviour(this, 5000));
+
+        System.out.println("QueenAgent " + getAID().getName() + " is ready. ID: " + this.id + " n: " + this.n);
     }
 
     protected void takeDown()
@@ -34,6 +66,10 @@ public class QueenAgent extends Agent
         ServiceDescription queenService = new ServiceDescription();
         queenService.setName(ServiceList.SRVC_QUEEN_NAME);
         queenService.setType(ServiceList.SRVC_QUEEN_TYPE);
+        Property propQueenId = new Property();
+        propQueenId.setName("ID");
+        propQueenId.setValue(this.id);
+        queenService.addProperties(propQueenId);
 
         DFAgentDescription agentDescription = new DFAgentDescription();
         agentDescription.setName(getAID());
@@ -70,29 +106,80 @@ public class QueenAgent extends Agent
 
         // Queen service template
         DFAgentDescription queenServiceTemplate = new DFAgentDescription();
+
         ServiceDescription sd = new ServiceDescription();
         sd.setType(ServiceList.SRVC_QUEEN_TYPE);
         sd.setName(ServiceList.SRVC_QUEEN_NAME);
+
+        // Want to find my predecessor and successor, if any
+        if (this.id == 0)
+        {
+            // Only find a successor (ID 1)
+            Property propQueenId = new Property();
+            propQueenId.setName("ID");
+            propQueenId.setValue(this.id+1);
+            sd.addProperties(propQueenId);
+        }
+        if (this.id == n-1)
+        {
+            // Only find predecessor
+            Property propQueenId = new Property();
+            propQueenId.setName("ID");
+            propQueenId.setValue(this.id-1);
+            sd.addProperties(propQueenId);
+        }
+        else
+        {
+            // Find both predecessor and successor
+            Property propQueenIdPred = new Property();
+            propQueenIdPred.setName("ID");
+            propQueenIdPred.setValue(this.id-1);
+            Property propQueenIdSucc = new Property();
+            propQueenIdSucc.setName("ID");
+            propQueenIdSucc.setValue(this.id+1);
+
+            sd.addProperties(propQueenIdPred);
+            sd.addProperties(propQueenIdSucc);
+        }
+
         queenServiceTemplate.addServices(sd);
 
         try
         {
             DFAgentDescription[] result = DFService.search(this, queenServiceTemplate);
 
-            // TODO Only find my predecessor and successor ... according to ID that is encoded in the name?
-
             for (int i = 0; i < result.length; ++i)
             {
                 foundQueens.add(result[i].getName());
             }
 
-            System.out.println(getName() + " - Found " + foundQueens.size() + " queens");
+            System.out.println(getName() + " - Found " + foundQueens.size() + " queens: " + foundQueens);
 
-            this.queens = foundQueens;
+            // TODO Save to predecessor and successor variables
         }
         catch (FIPAException fe)
         {
             fe.printStackTrace();
         }
     }
+
+    //region Behaviours
+
+    private class InitWakerBehaviour extends WakerBehaviour
+    {
+        public InitWakerBehaviour(Agent a, long timeout)
+        {
+            super(a, timeout);
+        }
+
+        @Override
+        protected void onWake()
+        {
+            super.onWake();
+
+            getQueens();
+        }
+    }
+
+    //endregion
 }
